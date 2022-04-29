aab28be12e5f1357bab04c5e7c1e8b7823670bd7
masukawa.kentaro <masukawa.kentaro@HL00881.local>
2022-04-29 22:47:00 +0900

    tfsteteファイルをS3バケットで管理するよう変更

--- config.tf
+++ config.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-bucket-maskenpa" # 作成したS3バケット
-    region = "ap-northeast-1"
-    key = "terraform.tfstate"
+    bucket  = "tfstate-bucket-maskenpa" # 作成したS3バケット
+    region  = "ap-northeast-1"
+    key     = "terraform.tfstate"
     encrypt = true
   }
 }
